182c0e7e0d84251cf65daaf4a5db1ea30b5c2a3c
Andres Rios <rianby64@gmail.com>
2016-01-10 22:45:24 +0300

    added a simple test example. Happy testing

--- JamesRandom.js
+++ JamesRandom.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* 
    +----------------------------------------------------------------------+
    |                            HEP Random                                |
@@ -39,125 +41,125 @@
    M. Fischler    - State-saving using only ints, for portability 4/12/05
 */
 
-  "use strict";
 
-  // Number of instances with automatic seed selection
-  var numberOfEngines = 0;
-  // Maximum index into the seed table
-  var maxIndex = 215;
+// Number of instances with automatic seed selection
+var numberOfEngines = 0;
+// Maximum index into the seed table
+var maxIndex = 215;
 
-  //constructor: JamesRandom,
-  function JamesRandom( args ){
-    this.u = [];
-    this.c;
-    this.cd;
-    this.cm;
-    this.i97;
-    this.j97; 
+//constructor: JamesRandom,
+function JamesRandom( args ){
+  var args = args || {};
+  this.u = [];
+  this.c;
+  this.cd;
+  this.cm;
+  this.i97;
+  this.j97; 
 
-    this.Name = function(){ return "HepJamesRandom"; };
+  this.Name = function(){ return "HepJamesRandom"; };
+  
+  // Returns a pseudo random number between 0 and 1 (excluding the end points)
+  this.Flat = function(){
+    var uni;
+
+    do {
+       uni = this.u[this.i97] - this.u[this.j97];
+       if ( uni < 0.0 ) uni++;
+       this.u[this.i97] = uni;
+       
+       if (this.i97 == 0) this.i97 = 96;
+       else this.i97--;
+       
+       if (this.j97 == 0) this.j97 = 96;
+       else this.j97--;
+       
+       this.c -= this.cd;
+       if (this.c < 0.0) this.c += this.cm;
+       
+       uni -= this.c;
+       if (uni < 0.0) uni += 1.0;
+    } while ( uni <= 0.0 || uni >= 1.0 );
     
-    // Returns a pseudo random number between 0 and 1 (excluding the end points)
-    this.Flat = function(){
-      var uni;
+    return uni;
+       
+  };
 
-      do {
-         uni = this.u[this.i97] - this.u[this.j97];
-         if ( uni < 0.0 ) uni++;
-         this.u[this.i97] = uni;
-         
-         if (this.i97 == 0) this.i97 = 96;
-         else this.i97--;
-         
-         if (this.j97 == 0) this.j97 = 96;
-         else this.j97--;
-         
-         this.c -= this.cd;
-         if (this.c < 0.0) this.c += this.cm;
-         
-         uni -= this.c;
-         if (uni < 0.0) uni += 1.0;
-      } while ( uni <= 0.0 || uni >= 1.0 );
-      
-      return uni;
-         
-    };
+  // Fills the array "vect" of specified size with flat random values.
+  this.FlatArray = function( size, vect ){
+    var i;
 
-    // Fills the array "vect" of specified size with flat random values.
-    this.FlatArray = function( size, vect ){
-      var i;
+    for (i=0; i<size; ++i) {
+      vect[i] = this.Flat();
+    }   
 
-      for (i=0; i<size; ++i) {
-        vect[i] = this.Flat();
-      }   
+  };
 
-    };
+  // Sets the state of the algorithm according to seed.
+  this.SetSeed = function( seed, dum ){
+    // The input value for "seed" should be within the range [0,900000000]
+    //
+    // Negative seeds result in serious flaws in the randomness;
+    // seeds above 900000000 are OK because of the %177 in the expression for i,
+    // but may have the same effect as other seeds below 900000000.
 
-    // Sets the state of the algorithm according to seed.
-    this.SetSeed = function( seed, dum ){
-      // The input value for "seed" should be within the range [0,900000000]
-      //
-      // Negative seeds result in serious flaws in the randomness;
-      // seeds above 900000000 are OK because of the %177 in the expression for i,
-      // but may have the same effect as other seeds below 900000000.
+    var m, n, mm; // integers
+    var s, t;
 
-      var m, n, mm; // integers
-      var s, t;
+    if( seed < 0 ){
+      var msg = "Seed for HepJamesRandom must be non-negative. Seed value supplied was" + seed + ". Using its absolute value instead.";
+      console.log( msg );
+      seed = -seed;
+    }
 
-      if( seed < 0 ){
-        var msg = "Seed for HepJamesRandom must be non-negative. Seed value supplied was" + seed + ". Using its absolute value instead.";
-        console.log( msg );
-        seed = -seed;
-      }
-
-      var ij = seed / 30082;
-      var kl = seed - ( 30082 * ij );
-      var i = ( ( ij / 177 ) % 177 ) + 2;
-      var j = ( ij % 177 ) + 2;
-      var k = ( ( kl / 169 ) % 178 ) + 1;
-      var l = kl % 169;
-      
-      this.theSeed = seed;
-
-      for( n = 1 ; n < 98; n++ ) {
-        s = 0.0;
-        t = 0.5;
-        for( m = 1 ; m < 25; m++ ) {
-          mm = ( ( (i*j) % 179 ) * k ) % 179;
-          i = j;
-          j = k;
-          k = mm;
-          l = ( 53 * l + 1 ) % 169;
-          if ( (l*mm % 64 ) >= 32 )
-            s += t;
-          t *= 0.5;
-        }
-        this.u.push( s );
-      }
-      this.c = 362436.0 / 16777216.0;
-      this.cd = 7654321.0 / 16777216.0;
-      this.cm = 16777213.0 / 16777216.0;
+    var ij = seed / 30082;
+    var kl = seed - ( 30082 * ij );
+    var i = ( ( ij / 177 ) % 177 ) + 2;
+    var j = ( ij % 177 ) + 2;
+    var k = ( ( kl / 169 ) % 178 ) + 1;
+    var l = kl % 169;
     
-      this.i97 = 96;
-      this.j97 = 32;
-    };
+    this.theSeed = seed;
 
-    // Sets the state of the zero terminated array of seeds. Only the first seed is used.
-    this.SetSeeds = function( seeds, dum ){
-      this.SetSeed( seeds? seeds: 19780503, 0 );
-      this.theSeeds = seeds;
-    };
+    for( n = 1 ; n < 98; n++ ) {
+      s = 0.0;
+      t = 0.5;
+      for( m = 1 ; m < 25; m++ ) {
+        mm = ( ( (i*j) % 179 ) * k ) % 179;
+        i = j;
+        j = k;
+        k = mm;
+        l = ( 53 * l + 1 ) % 169;
+        if ( (l*mm % 64 ) >= 32 )
+          s += t;
+        t *= 0.5;
+      }
+      this.u.push( s );
+    }
+    this.c = 362436.0 / 16777216.0;
+    this.cd = 7654321.0 / 16777216.0;
+    this.cm = 16777213.0 / 16777216.0;
+  
+    this.i97 = 96;
+    this.j97 = 32;
+  };
 
-    this.theSeed = args.seed || new Date().getDate(); 
+  // Sets the state of the zero terminated array of seeds. Only the first seed is used.
+  this.SetSeeds = function( seeds, dum ){
+    this.SetSeed( seeds? seeds: 19780503, 0 );
+    this.theSeeds = seeds;
+  };
 
-    this.SetSeed( this.theSeed, 0 )
-    this.SetSeeds( this.theSeed, 0 );
-  }
+  this.theSeed = args.seed || new Date().getDate(); 
 
-  JamesRandom.MarkerLen = 64; // Enough room to hold a begin or end marker.
+  this.SetSeed( this.theSeed, 0 )
+  this.SetSeeds( this.theSeed, 0 );
+}
 
-  JamesRandom.EngineName = function(){ return "HepJamesRandom"; };
-  JamesRandom.BeginTag = function(){ return "JamesRandom-Begin"; };
+JamesRandom.MarkerLen = 64; // Enough room to hold a begin or end marker.
+
+JamesRandom.EngineName = function(){ return "HepJamesRandom"; };
+JamesRandom.BeginTag = function(){ return "JamesRandom-Begin"; };
 
 
 
